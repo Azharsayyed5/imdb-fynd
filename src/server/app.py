6517599a95fd078bc4bd1c6a1ec84f6771249d91
Azharsayyed5 <55562412+Azharsayyed5@users.noreply.github.com>
2021-05-05 14:22:22 +0530

    update import

--- src/server/app.py
+++ src/server/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from server.imdb.routes.views import router as IMDBRouter
-from server.accounts.routes.views import router as AccountsRouter
+from .imdb.routes.views import router as IMDBRouter
+from .accounts.routes.views import router as AccountsRouter
 
 app = FastAPI()
 app.include_router(IMDBRouter, tags=["IMDB"], prefix="/v1/imbd")

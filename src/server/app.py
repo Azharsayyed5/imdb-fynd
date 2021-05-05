8a6112b781462409379f9192c437eb3c4f1f11cd
Azharsayyed5 <55562412+Azharsayyed5@users.noreply.github.com>
2021-05-05 14:48:14 +0530

    Update router

--- src/server/app.py
+++ src/server/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from .imdb.routes.views import router as IMDBRouter
+# from .imdb.routes.views import router as IMDBRouter
 from .accounts.routes.views import router as AccountsRouter
 
 app = FastAPI()
-app.include_router(IMDBRouter, tags=["IMDB"], prefix="/v1/imbd")
+# app.include_router(IMDBRouter, tags=["IMDB"], prefix="/v1/imbd")
 app.include_router(AccountsRouter, tags=["Accounts"], prefix="/v1/accounts")
 
 @app.get("/", tags=["Root"])

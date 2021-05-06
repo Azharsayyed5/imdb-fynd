d394ff0d5666a5e9bf08343338af347f30590a11
Azharsayyed5 <55562412+Azharsayyed5@users.noreply.github.com>
2021-05-06 15:44:33 +0530

    Update logger path

--- src/server/logger.py
+++ src/server/logger.py
@@ -2,15 +2,17 @@ import os
 import sys
 import logging
 from datetime import date
+from pathlib import Path
+sys.path.append(os.path.dirname(Path(os.path.abspath(__file__)).parent))
 
-LOG_DIRECTORY = "/var/log/"
+LOG_DIRECTORY = os.path.dirname(Path(os.path.abspath(__file__)).parent) + "\logs"
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 def logging_handler():
     
     today = date.today()
 
-    filename = f"root.logs.{today.strftime('%d-%m-%Y')}"
+    filename = f"\\root.logs.{today.strftime('%d-%m-%Y')}"
     if not os.path.exists(LOG_DIRECTORY):
         os.makedirs(LOG_DIRECTORY)
 
